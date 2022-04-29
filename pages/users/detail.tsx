4a003755a79031d71241f1b91a57507b2f295aa2
arfian-rp <arfianpradana07@gmail.com>
2022-04-29 19:42:20 +0700

    ok

---
+++ pages/users/detail.tsx
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <div>
+      <p>detail</p>
+    </div>
+  );
+};
---
+++ pages/users/index.tsx
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <div>
+      <p>users</p>
+    </div>
+  );
+};
